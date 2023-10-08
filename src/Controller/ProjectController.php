dc37ab9d32cfffa26751457b25cc97d4f46d698d
Tomas Gilvonauskas <tgilvonas@gmail.com>
2023-10-08 19:05:34 +0300

    Login/logout/protect scaffold has been done

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectController extends AbstractController
+{
+    #[Route('/projects', name: 'projects_index')]
+    public function index(): Response
+    {
+        return $this->render('project/index.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    #[Route('/projects/edit/{id}', name: 'projects_edit')]
+    public function edit(int $id): Response
+    {
+        dd('Projects edit');
+    }
+}
---
+++ src/Controller/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    #[Route('/tasks', name: 'tasks_index')]
+    public function index(): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+
+    #[Route('/tasks/edit/{id}', name: 'tasks_edit')]
+    public function edit(int $id): Response
+    {
+        dd('Tasks edit');
+    }
+}
