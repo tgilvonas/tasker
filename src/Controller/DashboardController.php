1391084440c0a5ddf164dcd935820e7026973352
Tomas Gilvonauskas <tgilvonas@gmail.com>
2023-10-31 21:24:12 +0200

    Dashboard was concluded with metric

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
+    protected ProjectRepository $projectRepository;
+
+    public function __construct(ProjectRepository $projectRepository)
+    {
+        $this->projectRepository = $projectRepository;
+    }
+
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
+        $paramsCountTotals = [
+            'total' => true,
+            'completed' => true,
+        ];
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'projectsCompletionData' => $this->projectRepository->getProjectsList($paramsCountTotals),
         ]);
     }
 }
