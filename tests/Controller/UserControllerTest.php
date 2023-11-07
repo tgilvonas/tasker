cdb6a99889f61b24615ea9c0f97aac152d3f63bb
Tomas Gilvonauskas <tgilvonas@gmail.com>
2023-11-07 12:27:33 +0200

    Tests are in progress

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -4,15 +4,17 @@ namespace App\Tests\Controller;
 
 class UserControllerTest extends GenericControllerTestCase
 {
-    public function testIndex(): void
+    public function testIndexAction(): void
     {
         $this->client->request('GET', '/users');
         $this->assertResponseRedirects($this->baseUrl . '/');
 
+        $this->client->loginUser($this->user);
+        $this->client->request('GET', '/users');
+        $this->assertResponseStatusCodeSame(403);
+
         $this->client->loginUser($this->superAdminUser);
-
         $crawler = $this->client->request('GET', '/users');
-
         $this->assertResponseIsSuccessful();
         $this->assertStringContainsString($this->superAdminUser->getEmail(), $crawler->text());
     }
