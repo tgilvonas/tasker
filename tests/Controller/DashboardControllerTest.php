fcc70dfa20950e17feedfcae0183d65840f9815f
Tomas Gilvonauskas <tgilvonas@gmail.com>
2023-11-07 12:19:36 +0200

    Tests are in progress

--- tests/Controller/DashboardControllerTest.php
+++ tests/Controller/DashboardControllerTest.php
@@ -13,5 +13,9 @@ class DashboardControllerTest extends GenericControllerTestCase
         $this->client->loginUser($this->superAdminUser);
         $this->client->request('GET', '/dashboard');
         $this->assertResponseIsSuccessful();
+
+        $this->client->loginUser($this->user);
+        $this->client->request('GET', '/dashboard');
+        $this->assertResponseIsSuccessful();
     }
 }
--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -19,6 +19,10 @@ class TaskControllerTest extends GenericControllerTestCase
         $this->client->request('GET', '/tasks');
         $this->assertResponseRedirects($this->baseUrl . '/');
 
+        $this->client->loginUser($this->user);
+        $this->client->request('GET', '/tasks');
+        $this->assertResponseIsSuccessful();
+
         $this->client->loginUser($this->superAdminUser);
         $this->client->request('GET', '/tasks');
         $this->assertResponseIsSuccessful();
@@ -30,6 +34,12 @@ class TaskControllerTest extends GenericControllerTestCase
         $this->assertResponseRedirects($this->baseUrl . '/');
 
         $this->client->followRedirects();
+
+        $this->client->loginUser($this->user);
+        $this->client->request('GET', '/tasks/create');
+        $this->assertResponseStatusCodeSame(403);
+        $this->client->request('POST', '/tasks/create');
+        $this->assertResponseStatusCodeSame(403);
 
         $this->client->loginUser($this->superAdminUser);
         $crawler = $this->client->request('GET', '/tasks/create');
@@ -48,10 +58,14 @@ class TaskControllerTest extends GenericControllerTestCase
     {
         $this->client->followRedirects();
 
-        $project = $this->taskRepository->findAll()[0];
+        $task = $this->taskRepository->findAll()[0];
+
+        $this->client->loginUser($this->user);
+        $this->client->request('GET', '/tasks/update/' . $task->getId());
+        $this->assertResponseIsSuccessful();
 
         $this->client->loginUser($this->superAdminUser);
-        $crawler = $this->client->request('GET', '/tasks/update/' . $project->getId());
+        $crawler = $this->client->request('GET', '/tasks/update/' . $task->getId());
         $this->assertResponseIsSuccessful();
 
         $buttonCrawlerNode = $crawler->selectButton('task_form[submit]');
@@ -62,7 +76,7 @@ class TaskControllerTest extends GenericControllerTestCase
         $crawler = $this->client->submit($form);
         $this->assertStringContainsString($this->translate('record_updated'), $crawler->text());
 
-        $crawler = $this->client->request('GET', '/tasks/update/' . $project->getId());
+        $crawler = $this->client->request('GET', '/tasks/update/' . $task->getId());
         $this->assertStringContainsString('Task 2', $crawler->text());
         $this->assertStringContainsString('This is the description of Task 2.', $crawler->text());
     }
@@ -72,6 +86,10 @@ class TaskControllerTest extends GenericControllerTestCase
         $task = $this->taskRepository->findAll()[0];
 
         $this->client->followRedirects();
+
+        $this->client->loginUser($this->user);
+        $this->client->request('POST', '/tasks/delete/' . $task->getId());
+        $this->assertResponseStatusCodeSame(403);
 
         $this->client->loginUser($this->superAdminUser);
         $crawler = $this->client->request('POST', '/tasks/delete/' . $task->getId());
